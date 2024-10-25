05430cd6c3f785ed6b294bafc286283178c12d5d
lucasgianine <lucas.esilva@sptech.school>
2024-10-25 15:20:49 -0300

    Centraliza função de postar mensagem no export handler

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import {
   BooleanRecordProperty,
+  Cogfy,
   QueryRecordsCommand,
   TextRecordProperty
 } from 'cogfy'
-import { cogfy, whapi } from './clients'
+import { whapi } from './clients'
 import { PostMessageTextBody } from './types'
 
 exports.handler = async (event: any, _context: any) => {
   const isCboRule = event.resources[0].includes('rule/cbo-clipping-scheduler')
 
-  const collectionId = isCboRule ? process.env.COGFY_CBO_COLLECTION_ID! : process.env.COGFY_SBC_COLLECTION_ID!
-  const apiKey = isCboRule ? process.env.COGFY_CBO_API_KEY! : process.env.COGFY_SBC_API_KEY!
+  const cogfy = new Cogfy({
+    apiKey: isCboRule ? process.env.COGFY_CBO_API_KEY! : process.env.COGFY_SBC_API_KEY!
+  })
 
   // Obter todos os records e verificar se o status é publicável
   const channelId = isCboRule ? process.env.CBO_CHANNEL_ID! : process.env.SBC_CHANNEL_ID!
+  const collectionId = isCboRule ? process.env.COGFY_CBO_COLLECTION_ID! : process.env.COGFY_SBC_COLLECTION_ID!
   const selectionFieldId = isCboRule ? process.env.COGFY_CBO_SELECTION_FIELD_ID! : process.env.COGFY_SBC_SELECTION_FIELD_ID!
   const optionSelectedId = isCboRule ? process.env.COGFY_CBO_OPTION_SELECTED_ID! : process.env.COGFY_SBC_OPTION_SELECTED_ID!
 
@@ -32,44 +35,33 @@ exports.handler = async (event: any, _context: any) => {
     }
   }
 
-  const recordsToPublish = (await cogfy.queryRecords(apiKey, collectionId, filter)).data
+  const recordsToPublish = (await cogfy.records.query(collectionId, filter)).data
 
   recordsToPublish.map(async record => {
     const bodyTextId = isCboRule ? process.env.COGFY_CBO_BODY_FIELD_ID! : process.env.COGFY_SBC_BODY_FIELD_ID!
-
     const body = (record.properties[bodyTextId] as TextRecordProperty).text.value!
 
-    await postMessage(apiKey, isCboRule, collectionId, record.id, channelId, body)
-  })
-}
+    const message: PostMessageTextBody = { to: channelId, body }
 
-async function postMessage (
-  apiKey: string,
-  isCboRule: boolean,
-  collectionId: string,
-  recordId: string,
-  channelId: string,
-  body: string
-): Promise<void> {
-  const message: PostMessageTextBody = { to: channelId, body }
-  try {
-    const response = await whapi.postMessageText(message)
+    try {
+      const response = await whapi.postMessageText(message)
 
-    if (response.sent) {
-      const booleanFieldId = isCboRule ? process.env.COGFY_CBO_BOOLEAN_FIELD_ID! : process.env.COGFY_SBC_BOOLEAN_FIELD_ID!
+      if (response.sent) {
+        const booleanFieldId = isCboRule ? process.env.COGFY_CBO_BOOLEAN_FIELD_ID! : process.env.COGFY_SBC_BOOLEAN_FIELD_ID!
 
-      const properties = {
-        [booleanFieldId]: {
-          type: 'boolean',
-          boolean: {
-            value: true
+        const properties = {
+          [booleanFieldId]: {
+            type: 'boolean',
+            boolean: {
+              value: true
+            }
           }
-        }
-      } as unknown as Record<string, BooleanRecordProperty>
+        } as unknown as Record<string, BooleanRecordProperty>
 
-      await cogfy.updateRecords(apiKey, collectionId, recordId, { properties })
+        await cogfy.records.update(collectionId, record.id, { properties })
+      }
+    } catch (err) {
+      throw new Error(err)
     }
-  } catch (err) {
-    throw new Error(err)
-  }
+  })
 }
